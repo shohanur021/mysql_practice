0f33043fc0284ac4c92ab1cf5130fa68abd25b54
shohanur021 <shohanur021@gmail.com>
2025-08-15 20:09:36 +0600

    sixth time commit

--- practice_7.sql
+++ practice_7.sql
@@ -106,16 +106,23 @@ hour('2025-01-05 13:22:04') as hour3,
 minute('2025-01-05 13:22:04') as minute3,
 second('2025-01-05 13:22:04') as second3,
 datediff('2025-05-01','2025-01-05') as difference_day,
-datediff('2025-09-01 23:22:04','2025-02-25 11:14:00') as difference_day2,
-timestampdiff(hour,'2025-02-25 11:14:00','2025-09-01 23:22:04') as hour_diffarence,
+datediff('2025-09-01 23:22:04','2025-02-25 11:14:00') as difference_day2;
+
+select
+timestampdiff(hour,'2025-02-25 11:14:00','2025-09-01 23:22:04') as hour_diffarence,  -- datetime1:'2025-02-25 11:14:00' & datetime2:'2025-09-01 23:22:04' (datetime2-datetime1)
 timestampdiff(minute,'2025-02-25 11:14:00','2025-09-01 23:22:04') as minute_diffarence,
-timestampdiff(second,'2025-02-25 11:14:00','2025-09-01 23:22:04') as second_diffarence;
+timestampdiff(second,'2025-02-25 11:14:00','2025-09-01 23:22:04') as second_diffarence,
+timestampdiff(day,'2025-02-25 11:14:00','2025-09-01 23:22:04') as day_diffarence,
+timestampdiff(month,'2025-02-25 11:14:00','2025-09-01 23:22:04') as month_diffarence,
+timestampdiff(year,'2025-02-25 11:14:00','2029-09-01 23:22:04') as year_diffarence;
 
 select
 date_format('2025-02-25 11:14:00', '%W') as formated_date_week,
-date_format('2025-02-25 11:14:00', '%M') as formated_date_month,
-date_format('2025-02-25 11:14:00', '%e') as formated_date_day,
-date_format('2025-02-25 11:14:00', '%e') as formated_date_year,
+date_format('2025-02-25 11:14:00', '%M') as formated_date_month,   --Give the result as: January, February, March, April.....
+date_format('2025-02-25 11:14:00', '%m') as formated_date_month2,   --Give the result as: 01, 02, 03, 04 .......
+date_format('2025-02-25 11:14:00', '%e') as formated_date_day,     --Give total days of the month without leading zeros
+date_format('2025-02-25 11:14:00', '%d') as formated_date_day2,   --Give total days of the month with leading zeros
+date_format('2025-02-25 11:14:00', '%Y') as formated_date_year,
 date_format('2025-02-25 11:14:00', '%W, %M %e, %Y') as formated_date;
 
 select
@@ -123,3 +130,32 @@ date_format('2025-02-25 21:14:00', '%H') as formated_hour,
 date_format('2025-02-25 21:14:00', '%i') as formated_minute,
 date_format('2025-02-25 21:14:36', '%s') as formated_second,
 date_format('2025-02-25 21:14:36', '%H %i %s') as formated_second;
+
+
+-- check is it a date or not a date
+select 
+str_to_date('123','%Y-%M-%e') is not null as date_check1,   --Returns 1 if it is a valid date, 0 if it is not a valid date.  
+str_to_date('2025','%Y-%M-%e') is not null as date_check2,
+str_to_date('2025-02-25','%Y-%M-%e') is not null as date_check3,
+str_to_date('2025-12-36','%Y-%M-%e') is not null as date_check4,
+str_to_date('2025-12-18','%Y-%m-%d') is not null as date_check5;       
+
+
+
+select 
+orderid,
+productid,
+shipaddress,
+orderdate,
+date_format(orderdate,'%M') as month_name
+from orders
+order by month(orderdate) desc;
+
+
+
+select 
+orderid,
+customerid,
+shipaddress,
+timestampdiff(day, orderdate, shipdate) as day_need_shipping
+from  orders_archive;
